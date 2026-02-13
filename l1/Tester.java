9d4e3a53933c73334a016e7a3518aa77e0891969
fayjullah haque <fayjullah.h.emon@gmail.com>
2026-02-13 12:27:29 +0600

    value assign

--- l1/Tester.java
+++ l1/Tester.java
@@ -4,5 +4,21 @@ public class Tester{
         Student s2 = new Student();
         System.out.println(s1); //s1 store the address of the object
         System.out.println(s2.name); //s2.name is null because we haven't assigned any value to it.
+
+        s1.name = "ALice";
+        s1.id = "5001";
+        s1.age = 20;
+
+        s2.name = "Bob";
+        s2.id = "5002";
+        s2.age = 22;
+
+        System.out.println(s1.name); 
+        System.out.println(s1.age); 
+        System.out.println(s1.id); 
+        System.out.println(s2.name); 
+        System.out.println(s2.id); 
+        System.out.println(s2.age); 
+        
     }
 }
